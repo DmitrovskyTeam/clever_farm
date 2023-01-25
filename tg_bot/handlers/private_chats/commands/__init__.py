86e1d88e0125494c8679621cdee831ea17b29d3d
vitaliy <vv.khritkin@gmail.com>
2023-01-25 16:40:42 +0300

    add commands templates

--- tg_bot/handlers/private_chats/commands/__init__.py
+++ tg_bot/handlers/private_chats/commands/__init__.py
@@ -1,2 +1,6 @@
 from . import start
+from . import air_temp
+from . import air_hum
+from . import ground_hum
+from . import control
 from . import help
--- tg_bot/handlers/private_chats/commands/control.py
+++ tg_bot/handlers/private_chats/commands/control.py
@@ -1,10 +1,10 @@
 from aiogram import types
-from aiogram.dispatcher.filters import CommandHelp
 
+from tg_bot.filters.commands import CommandControl
 from tg_bot.loader import dp
 
 
-@dp.message_handler(CommandHelp(), chat_type='private')
+@dp.message_handler(CommandControl(), chat_type='private')
 async def air_temp_request(message: types.Message):
     await dp.bot.delete_message(chat_id=message.chat.id,
                                 message_id=message.message_id)

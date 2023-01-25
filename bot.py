e5d5c5b7a39bd486c7f5fd796576d78cc3343dc6
vitaliy <vv.khritkin@gmail.com>
2023-01-25 16:05:48 +0300

    add start command

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_shutdown(dispatcher):
     await on_shutdown_notify(dispatcher)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dispatcher=dp,
-                           on_startup=on_startup,
-                           on_shutdown=on_shutdown)
+# if __name__ == '__main__':
+executor.start_polling(dispatcher=dp,
+                       on_startup=on_startup,
+                       on_shutdown=on_shutdown)

73d9a1dc8ef37624e4840ee763fa1163fa106349
vitaliy <vv.khritkin@gmail.com>
2023-01-31 14:07:19 +0300

    add tests

--- tests/farm_api_module_tests.py
+++ tests/farm_api_module_tests.py
@@ -1,5 +1,7 @@
 import unittest
 
+import requests
+
 from farm_api_module import FarmApiModule
 
 
@@ -10,39 +12,117 @@ class BasicModuleTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.api_module.close()
 
-    def test_temperature1(self):
-        self.assertEqual(None, self.api_module.get_air_temp_hum(5))
+    def test_temperature(self):
+        self.assertEqual(None, self.api_module.get_air_temp_hum(sensor_id=5))
+        self.assertEqual(None, self.api_module.get_air_temp_hum(sensor_id=-1))
+        self.assertEqual(None, self.api_module.get_air_temp_hum(sensor_id=0))
+        self.assertNotEqual(None, self.api_module.get_air_temp_hum(sensor_id=1))
+        self.assertNotEqual(None, self.api_module.get_air_temp_hum(sensor_id=4))
+        for i in range(1, 5):
+            response = str(self.api_module.get_air_temp_hum(sensor_id=i))
+            self.assertIn('id', response)
+            self.assertIn('temperature', response)
+            self.assertIn('humidity', response)
 
-    def test_temperature2(self):
-        self.assertEqual(None, self.api_module.get_air_temp_hum(-1))
+    def test_ground_sensor(self):
+        self.assertEqual(None, self.api_module.get_ground_hum(sensor_id=-1))
+        self.assertEqual(None, self.api_module.get_ground_hum(sensor_id=0))
+        self.assertEqual(None, self.api_module.get_ground_hum(sensor_id=7))
+        self.assertNotEqual(None, self.api_module.get_ground_hum(sensor_id=1))
+        self.assertNotEqual(None, self.api_module.get_ground_hum(sensor_id=6))
+        for i in range(1, 7):
+            response = str(self.api_module.get_ground_hum(sensor_id=i))
+            self.assertIn('id', response)
+            self.assertIn('humidity', response)
 
-    def test_temperature3(self):
-        self.assertEqual(None, self.api_module.get_air_temp_hum(0))
+    def test_hydration_system(self):
+        self.assertDictEqual({'code': 200, 'message': 'State success change. Hydration success start'},
+                             self.api_module.control_humidity_system(state=1))
+        self.assertDictEqual({'code': 200, 'message': 'State success change. Hydration success end'},
+                             self.api_module.control_humidity_system(state=0))
 
-    def test_temperature4(self):
-        self.assertIn('id', str(self.api_module.get_air_temp_hum(1)))
-        self.assertIn('temperature', str(self.api_module.get_air_temp_hum(1)))
-        self.assertIn('humidity', str(self.api_module.get_air_temp_hum(1)))
+        session = requests.Session()
+        self.assertDictEqual({'code': 404, 'message': 'State value not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/total_hum',
+                                           params={
+                                               'state': str(-1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/total_hum').json())
+        self.assertEqual(None, self.api_module.control_humidity_system(state=-1))
+        self.assertEqual(None, self.api_module.control_humidity_system(state=2))
+        session.close()
 
-    def test_temperature5(self):
-        self.assertNotEqual(None, self.api_module.get_air_temp_hum(1))
+    def test_window_system(self):
+        self.assertDictEqual({'code': 200, 'message': 'State success change. Window success open'},
+                             self.api_module.control_windows(state=1))
+        self.assertDictEqual({'code': 200, 'message': 'State success change. Window success close'},
+                             self.api_module.control_windows(state=0))
 
-    def test_temperature6(self):
-        self.assertNotEqual(None, self.api_module.get_air_temp_hum(4))
-    def test_ground_sensor1(self):
-        self.assertEqual(None, self.api_module.get_ground_hum(-1))
+        session = requests.Session()
+        self.assertDictEqual({'code': 404, 'message': 'State value not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/fork_drive',
+                                           params={
+                                               'state': str(-1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/fork_drive').json())
+        self.assertEqual(None, self.api_module.control_windows(state=-1))
+        self.assertEqual(None, self.api_module.control_windows(state=2))
+        session.close()
 
-    def test_ground_sensor2(self):
-        self.assertEqual(None, self.api_module.get_ground_hum(0))
-
-    def test_ground_sensor3(self):
-        self.assertEqual(None, self.api_module.get_ground_hum(7))
-
-    def test_ground_sensor4(self):
-        self.assertNotEqual(None, self.api_module.get_ground_hum(1))
-
-    def test_ground_sensor5(self):
-        self.assertNotEqual(None, self.api_module.get_ground_hum(6))
+    def test_control_watering(self):
+        self.assertEqual(None, self.api_module.control_watering(pomp_id=0, state=-1))
+        self.assertEqual(None, self.api_module.control_watering(pomp_id=1, state=-1))
+        self.assertDictEqual({'code': 200, 'message': 'State success change. Watering success open'},
+                             self.api_module.control_watering(pomp_id=1, state=1))
+        self.assertDictEqual({'code': 200, 'message': 'State success change. Watering success close'},
+                             self.api_module.control_watering(pomp_id=1, state=0))
+        self.assertEqual(None, self.api_module.control_watering(pomp_id=-1, state=1))
+        session = requests.Session()
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(0),
+                                               'state': str(-1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(3),
+                                               'state': str(-1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(10),
+                                               'state': str(-1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(10),
+                                               'state': str(-1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(10),
+                                               'state': str(0)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(0),
+                                               'state': str(1)
+                                           }).json())
+        self.assertDictEqual({'code': 404, 'message': 'State value or id not found'},
+                             session.patch(url='https://dt.miet.ru/ppo_it/api/watering',
+                                           params={
+                                               'id': str(1),
+                                               'state': str(10)
+                                           }).json())
+        session.close()
 
 
 if __name__ == "__main__":

98bdce99ea0fac298d96371e604a1316d3efdf36
vitaliy <vv.khritkin@gmail.com>
2023-01-31 11:29:15 +0300

    add role filer

--- tg_bot/filters/__init__.py
+++ tg_bot/filters/__init__.py
@@ -1,1 +1,8 @@
-from . import commands
+from .commands import CommandAirTemp, CommandControl, CommandAirHum, CommandGroundHum
+from .role_filter import RoleFilter
+from ..loader import dp
+
+if __name__ == "filters":
+    dp.filters_factory.bind(RoleFilter)
+
+__all__ = [CommandAirTemp, CommandControl, CommandAirHum, CommandGroundHum, RoleFilter]

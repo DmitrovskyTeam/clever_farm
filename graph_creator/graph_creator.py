b6b80244014c5a94533681c48df73d75dba75ac3
vitaliy <vv.khritkin@gmail.com>
2023-02-02 10:47:26 +0300

    fix air_temp command and graph creator

--- graph_creator/graph_creator.py
+++ graph_creator/graph_creator.py
@@ -8,10 +8,6 @@ class GraphCreator:
     def create_graph(self, data: list, output_filename: str):
         self.creator.figure(figsize=(20, 20))
         for i in range(0, len(data)):
-            print(data[i].get('label'))
-            print(i + 1)
-            print(data[i].get('data_x'))
-            print(data[i].get('data_y'))
             self.creator.subplot(len(data), 1, i + 1)
             self.creator.plot(data[i].get('data_x'), data[i].get('data_y'))
             self.creator.title(data[i].get('label'))

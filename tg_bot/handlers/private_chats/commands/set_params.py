6800d8f055a402cc4c145a2dce96d0881b292967
vitaliy <vv.khritkin@gmail.com>
2023-02-07 11:58:14 +0300

    complete setting system params

--- tg_bot/handlers/private_chats/commands/set_params.py
+++ tg_bot/handlers/private_chats/commands/set_params.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery
 
 from tg_bot.filters import CommandSetParams
@@ -18,45 +19,49 @@ async def set_params_handler_admin(message: types.Message):
 @dp.callback_query_handler(set_params_callback.filter(param='request_timeout'),
                            chat_type='private',
                            role_filter='admin')
-async def set_param_request_timeout(call: CallbackQuery):
-    await dp.bot.edit_message_text(chat_id=call.message.chat.id,
+async def set_param_request_timeout(call: CallbackQuery, state: FSMContext):
+    red_message: types.Message = await dp.bot.edit_message_text(chat_id=call.message.chat.id,
                                    message_id=call.message.message_id,
                                    reply_markup=None,
                                    text='Введите время в секундах для задания нового интервала запросов данных с датчиков в теплице')
     await SetParamsStates.set_request_timeout.set()
+    await state.update_data(red_message=red_message.message_id)
+    await state.update_data(param_value='request_timeout')
 
 
 @dp.callback_query_handler(set_params_callback.filter(),
                            chat_type='private',
                            role_filter='admin')
-async def set_param_current_param(call: CallbackQuery, callback_data: dict):
+async def set_param_current_param(call: CallbackQuery, callback_data: dict, state: FSMContext):
     param = callback_data.get('param')
     arg = callback_data.get('arg')
     if param == 'air_temp':
         if arg == 'min':
             param_value = '<b>Минимальная температура воздуха</b>'
             await SetParamsStates.set_min_air_temp.set()
-        if arg == 'min':
+        if arg == 'max':
             param_value = '<b>Максимальная температура воздуха</b>'
             await SetParamsStates.set_max_air_temp.set()
     elif param == 'air_hum':
         if arg == 'min':
             param_value = '<b>Минимальная влажность воздуха</b>'
             await SetParamsStates.set_min_air_hum.set()
-        if arg == 'min':
+        if arg == 'max':
             param_value = '<b>Максимальная влажность воздуха</b>'
             await SetParamsStates.set_max_air_hum.set()
     elif param == 'ground_hum':
         if arg == 'min':
             param_value = '<b>Минимальная влажность почвы</b>'
             await SetParamsStates.set_min_ground_hum.set()
-        if arg == 'min':
+        if arg == 'max':
             param_value = '<b>Максимальная влажность почвы</b>'
             await SetParamsStates.set_max_ground_hum.set()
-    await dp.bot.edit_message_text(chat_id=call.message.chat.id,
-                                   message_id=call.message.message_id,
-                                   reply_markup=None,
-                                   text=f'Введите новое значение для параметра {param_value}')
+    red_message: types.Message = await dp.bot.edit_message_text(chat_id=call.message.chat.id,
+                                                                message_id=call.message.message_id,
+                                                                reply_markup=None,
+                                                                text=f'Введите новое значение для параметра {param_value}')
+    await state.update_data(red_message=red_message.message_id)
+    await state.update_data(param_value=param_value)
 
 
 @dp.callback_query_handler(set_params_callback.filter(param='no_action'))

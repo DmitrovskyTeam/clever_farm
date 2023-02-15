17ccd469caa7d78852ae6eb95d9bcecc19ac5f07
vitaliy <vv.khritkin@gmail.com>
2023-02-15 09:41:26 +0300

    fix notify admins

--- tg_bot/utils/notify_admins.py
+++ tg_bot/utils/notify_admins.py
@@ -9,15 +9,13 @@ async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
             await dp.bot.send_message(admin, "Бот запущен")
-
-        except Exception as err:
-            logging.exception(err)
+        except Exception:
+            pass
 
 
 async def on_shutdown_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
             await dp.bot.send_message(admin, "Бот остановлен")
-
-        except Exception as err:
-            logging.exception(err)
+        except Exception:
+            pass

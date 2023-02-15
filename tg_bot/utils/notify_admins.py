e0f6ed427a3e507b737acc43ce5d18ca876b24b5
vitaliy <vv.khritkin@gmail.com>
2023-02-15 09:38:35 +0300

    fix main branch

---
+++ tg_bot/utils/notify_admins.py
@@ -0,0 +1,23 @@
+import logging
+
+from aiogram import Dispatcher
+
+from data import ADMINS
+
+
+async def on_startup_notify(dp: Dispatcher):
+    for admin in ADMINS:
+        try:
+            await dp.bot.send_message(admin, "Бот запущен")
+
+        except Exception as err:
+            logging.exception(err)
+
+
+async def on_shutdown_notify(dp: Dispatcher):
+    for admin in ADMINS:
+        try:
+            await dp.bot.send_message(admin, "Бот остановлен")
+
+        except Exception as err:
+            logging.exception(err)
